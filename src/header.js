62b5310290158e4a4ea40e190d53198ddb747ab1
muralirajCSE29 <109712380+muralirajCSE29@users.noreply.github.com>
2023-05-26 18:16:13 +0530

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 import img2 from "./assets/Bg-img.png";
-import bgimg from "./assets/sea.jpg";
-import './App.css';
+import bgimg from "./assets/bgmainsea.jpg";
+import "./App.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import { FaFacebook, FaLinkedin, FaInstagram, FaSearch } from "react-icons/fa";
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 function Header() {
   const [countdown, setCountdown] = useState(null);
   useEffect(() => {
-    const targetDate = new Date('2023-09-16');
+    const targetDate = new Date("2023-09-16");
     const interval = setInterval(() => {
       const now = new Date();
       const difference = targetDate - now;
@@ -28,44 +28,79 @@ function Header() {
   return (
     <div className="bg-emrald">
       <div className="overflow-x-auto">
-        <div className="flex flex-col items-center justify-center relative min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${bgimg})` }}>
+        <div
+          className="flex flex-col items-center justify-center relative min-h-screen bg-cover bg-center"
+          style={{ backgroundImage: `url(${bgimg})` }}
+        >
           <div className="flex flex-col justify-center items-center p-9 mx-auto">
             <div className="flex flex-col items-center space-y-4 md:flex-row md:justify-between md:space-y-0 md:mb-8 lg:space-x-4">
-            <div className="flex items-center space-x-2">
-  <a href="#" className="text-blue1 text-2xl md:text-3xl lg:text-4xl">
-    <FaFacebook />
-  </a>
-  <a href="#" className="text-blue1 text-2xl md:text-3xl lg:text-4xl">
-    <FaInstagram />
-  </a>
-  <a href="#" className="text-blue1 text-2xl md:text-3xl lg:text-4xl">
-    <FaLinkedin />
-  </a>
-</div>
+              <div className="flex items-center space-x-2">
+                <a
+                  href="https://www.facebook.com/Kreativdistrikt/"
+                  className="text-yellow text-2xl md:text-3xl lg:text-4xl"
+                >
+                  <FaFacebook />
+                </a>
+                <a
+                  href="https://www.instagram.com/kreativdistrikt/"
+                  className="text-yellow text-2xl md:text-3xl lg:text-4xl"
+                >
+                  <FaInstagram />
+                </a>
+                <a
+                  href="#"
+                  className="text-yellow text-2xl md:text-3xl lg:text-4xl"
+                >
+                  <FaLinkedin />
+                </a>
+              </div>
 
-<div className="flex items-center space-x-2">
-  <input type="text" className="p-2 bg-emrald rounded-md  hover:border-blue focus:border-blue md:text-3xl" placeholder="Search" />
-  <button className="p-2 bg-white rounded-md "><FaSearch /></button>
-</div>
-
+              <div className="flex items-center space-x-2">
+                <input
+                  type="text"
+                  className="p-2 bg-emrald rounded-md  hover:border-blue focus:border-blue md:text-3xl"
+                  placeholder="Search"
+                />
+                <button className="p-2 bg-emrald rounded-md ">
+                  <FaSearch />
+                </button>
+              </div>
             </div>
-            <img className="object-top mx-auto mt-12 w-3/4 md:w-full lg:w-1/2" src={img2} alt="Ocean" />
-            <h1 className="p-8 text-center text-white text-4xl md:text-6xl lg:text-8xl font-extrabold">DECODE THE OCEAN!</h1>
-            <h1 className="p-8 text-center text-white text-2xl md:text-3xl lg:text-4xl font-extrabold">7, 8 &amp; 9 February 2022</h1>
-            <h1 className="text-center text-white text-xl md:text-2xl lg:text-3xl font-extrabold">GEAR UP AT YOUR OWN PACE OVER A 3-WEEK IDEATION JOURNEY, JOIN</h1>
-            <span className="text-center text-white text-xl md:text-2xl lg:text-3xl font-extrabold">A 3-DAY VIRTUAL HACKATHON AND WIN UP TO <span className="text-yellow">€60,000 IN PRIZES!</span></span>
+            <img
+              className="object-top mx-auto mt-12 w-3/4 md:w-full lg:w-1/2"
+              src={img2}
+              alt="Ocean"
+            />
+            <h1 className="p-8 text-center text-white text-4xl md:text-6xl lg:text-8xl font-extrabold">
+              DECODE THE OCEAN!
+            </h1>
+            <h1 className="p-8 text-center text-white text-2xl md:text-3xl lg:text-4xl font-extrabold">
+              7, 8 &amp; 9 February 2022
+            </h1>
+            <h1 className="text-center text-white text-xl md:text-2xl lg:text-3xl font-extrabold">
+              GEAR UP AT YOUR OWN PACE OVER A 3-WEEK IDEATION JOURNEY, JOIN
+            </h1>
+            <span className="text-center text-white text-xl md:text-2xl lg:text-3xl font-extrabold">
+              A 3-DAY VIRTUAL HACKATHON AND WIN UP TO{" "}
+              <span className="text-yellow">€60,000 IN PRIZES!</span>
+            </span>
             {countdown && (
               <div className="text-midnight text-xl md:text-2xl lg:text-3xl font-extrabold mt-8">
-                Countdown: {countdown.days} days, {countdown.hours} hours, {countdown.minutes} minutes, {countdown.seconds} seconds
+                Countdown: {countdown.days} days, {countdown.hours} hours,{" "}
+                {countdown.minutes} minutes, {countdown.seconds} seconds
               </div>
             )}
             <div className="flex space-x-4 mt-8">
-            <Link to="/login">
-              <button className="p-2 bg-blue  border border-white text-3xl font-bold rounded-md focus:outline-none">Login</button>
+              <Link to="/login">
+                <button className="p-2 bg-bluelite  border border-yellow text-3xl font-bold rounded-md focus:outline-none">
+                  Login
+                </button>
               </Link>
 
               <Link to="/signup">
-              <button className="p-2 bg-emrald1 border border-white text-3xl font-bold rounded-md focus:outline-none">Sign Up</button>
+                <button className="p-2 bg-red border border-yellow text-3xl font-bold rounded-md focus:outline-none">
+                  Sign Up
+                </button>
               </Link>
             </div>
           </div>
