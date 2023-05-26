2a88b849574fa1c990b19183da3e11c15e1bd14e
muralirajCSE29 <109712380+muralirajCSE29@users.noreply.github.com>
2023-05-26 18:17:51 +0530

    Update AllComps.jsx

--- src/AllComps.jsx
+++ src/AllComps.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Header from './header'
-import About from './About'
-import Participate from './Participate'
-import Categories from './Categories'
-import Prize from './Prize'
-import Footer from './Footer'
+import React from "react";
+import Header from "./header";
+import About from "./About";
+import Participate from "./Participate";
+import Categories from "./Categories";
+import Prize from "./Prize";
+import Footer from "./Footer";
+import Footerbefore from "./Footerbefore";
 
 const AllComps = () => {
   return (
     <div>
-        <Header />
+      <Header />
       <About />
       <Participate />
       <Categories />
       <Prize />
+      <Footerbefore />
       <Footer />
+      
     </div>
-  )
-}
+  );
+};
 
-export default AllComps
+export default AllComps;
