eee439ca06959c804bcb95dfe72a4a5be72591d0
muralirajCSE29 <109712380+muralirajCSE29@users.noreply.github.com>
2023-05-26 18:17:13 +0530

    Update Prize.js

--- src/Prize.js
+++ src/Prize.js
@@ -7,73 +7,89 @@ import Blueprize from "./assets/Blue-skies-award.png";
 
 function Prize() {
   return (
-    <div className="bg-purple min-h-screen flex flex-col justify-start items-center py-12">
+    <div className="bg-rose min-h-screen flex flex-col justify-start items-center py-12">
       <img className="object-top mx-auto" src={prizelogo} alt="Prize-Pool" />
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-8">
           <div className="flex flex-col items-center">
-            <img className="object-top p-8 mx-auto" src={firstprize} alt="Prize Pool 1" />
+            <img
+              className="object-top p-8 mx-auto"
+              src={firstprize}
+              alt="Prize Pool 1"
+            />
             <div className="p-4">
               <h2 className="text-violet font-extrabold text-3xl sm:text-5xl text-center mb-6">
                 €25,000
               </h2>
               <p className="text-violet font-extrabold text-center text-lg">
                 Towards further evolving and giving outreach to the winning
-                solution, promoting its uptake. 2 "innovation" coaching
-                sessions to guide Team in evolving the solution. Travel costs
-                covered for up to 2 Team members to participate in Blue-Cloud
-                Final Conference.
+                solution, promoting its uptake. 2 "innovation" coaching sessions
+                to guide Team in evolving the solution. Travel costs covered for
+                up to 2 Team members to participate in Blue-Cloud Final
+                Conference.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center">
-            <img className="object-top p-8 mx-auto" src={secondprize} alt="Prize Pool 2" />
+            <img
+              className="object-top p-8 mx-auto"
+              src={secondprize}
+              alt="Prize Pool 2"
+            />
             <div className="p-4">
               <h2 className="text-violet font-extrabold text-3xl sm:text-5xl text-center mb-6">
                 €20,000
               </h2>
               <p className="text-violet font-extrabold text-center text-lg">
                 Towards further evolving and giving outreach to the winning
-                solution, promoting its uptake. 2 "innovation" coaching
-                sessions to guide Team in evolving the solution. Travel costs
-                covered for up to 2 Team members to participate in Blue-Cloud
-                Final Conference.
+                solution, promoting its uptake. 2 "innovation" coaching sessions
+                to guide Team in evolving the solution. Travel costs covered for
+                up to 2 Team members to participate in Blue-Cloud Final
+                Conference.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center">
-            <img className="object-top p-8 mx-auto" src={thirdprize} alt="Prize Pool 3" />
+            <img
+              className="object-top p-8 mx-auto"
+              src={thirdprize}
+              alt="Prize Pool 3"
+            />
             <div className="p-4">
               <h2 className="text-violet font-extrabold text-3xl sm:text-5xl text-center mb-6">
                 €15,000
               </h2>
               <p className="text-violet font-extrabold text-center text-lg">
                 Towards further evolving and giving outreach to the winning
-                solution, promoting its uptake. 2 "innovation" coaching
-                sessions to guide Team in evolving the solution. Travel costs
-                covered for up to 2 Team members to participate in Blue-Cloud
-                Final Conference.
+                solution, promoting its uptake. 2 "innovation" coaching sessions
+                to guide Team in evolving the solution. Travel costs covered for
+                up to 2 Team members to participate in Blue-Cloud Final
+                Conference.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center">
-            <img className="object-top p-8 mx-auto" src={Blueprize} alt="Prize Pool 4" />
+            <img
+              className="object-top p-8 mx-auto"
+              src={Blueprize}
+              alt="Prize Pool 4"
+            />
             <div className="p-4">
               <h2 className="text-violet font-extrabold text-3xl sm:text-5xl text-center mb-6">
                 Blue Skies
               </h2>
               <p className="text-violet font-extrabold text-center text-lg">
-                Granted by "popular vote" of participants that join the "Pitch
-                & Award Event". Travel costs covered for up to 2 Team members
-                to participate in Blue-Cloud Final Conference.
+                Granted by "popular vote" of participants that join the "Pitch &
+                Award Event". Travel costs covered for up to 2 Team members to
+                participate in Blue-Cloud Final Conference.
               </p>
             </div>
           </div>
         </div>
         <h1 className="text-violet font-bold text-center text-2xl mt-12">
-          The Winners commit to investing the awarded prize to further
-          develop, boost, and promote their solutions as explained in the
-          applicable Terms & Conditions.
+          The Winners commit to investing the awarded prize to further develop,
+          boost, and promote their solutions as explained in the applicable
+          Terms & Conditions.
         </h1>
       </div>
     </div>
