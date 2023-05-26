807ebc8c22ba69ccedb14ffd40f09e076d862d7e
muralirajCSE29 <109712380+muralirajCSE29@users.noreply.github.com>
2023-05-26 18:18:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Router, Routes } from "react-router-dom";
 // import About from './About';
-import './App.css';
+import "./App.css";
 // import Categories from './Categories';
 // import Footer from './Footer';
-import LoginPage from './Login';
+import LoginPage from "./Login";
 // import Participate from './Participate';
 // import Prize from './Prize';
 // import Header from "./header";
-import AllComps from './AllComps';
-import Signup from './Signup';
-import Forgotpassword from './Forgotpassword';
-
+import AllComps from "./AllComps";
+import Signup from "./Signup";
+import Forgotpassword from "./Forgotpassword";
 
 function App() {
   return (
-    
-    <div  >
+    <div>
       <Routes>
+        <Route path="/" element={<AllComps />} />
 
-      <Route path='/' element={<AllComps />} />
-      
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/forgotpassword' element={<Forgotpassword />} />
-
-
-      
-
-
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<Forgotpassword />} />
+        <Route path="/login" element={<Signup />} />
       </Routes>
-      
     </div>
-    
-    
   );
 }
 
